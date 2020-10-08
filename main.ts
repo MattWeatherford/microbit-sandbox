7d15c9dabc2fcd6ff056665d8bc384c2032e9ea5
Matt W <MattWeatherford@users.noreply.github.com>
2020-10-08 16:33:10 -0700

    loop logic added, startup logic added

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    IndexNumber += 1
+    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    IndexNumber += -1
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+})
+let IndexNumber = 0
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
 basic.forever(function () {
-	
+    if (IndexNumber > 12) {
+        IndexNumber = 1
+    } else if (IndexNumber < 1) {
+        IndexNumber = 12
+    } else {
+    	
+    }
 })
