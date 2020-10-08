e09a2e1770508bc1926432a2e0a23c4f1b2987f0
Matt W <MattWeatherford@users.noreply.github.com>
2020-10-08 16:57:56 -0700

    added 12 conditionals for different led's depending on index

--- main.ts
+++ main.ts
@@ -19,7 +19,37 @@ basic.forever(function () {
         IndexNumber = 1
     } else if (IndexNumber < 1) {
         IndexNumber = 12
+    } else if (IndexNumber == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else if (IndexNumber == 2) {
+        basic.showIcon(IconNames.Sad)
+    } else if (IndexNumber == 3) {
+        basic.showIcon(IconNames.Confused)
+    } else if (IndexNumber == 4) {
+        basic.showIcon(IconNames.Angry)
+    } else if (IndexNumber == 5) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (IndexNumber == 6) {
+        basic.showIcon(IconNames.Surprised)
+    } else if (IndexNumber == 7) {
+        basic.showIcon(IconNames.Silly)
+    } else if (IndexNumber == 8) {
+        basic.showIcon(IconNames.Fabulous)
+    } else if (IndexNumber == 9) {
+        basic.showIcon(IconNames.Meh)
+    } else if (IndexNumber == 10) {
+        basic.showIcon(IconNames.TShirt)
+    } else if (IndexNumber == 11) {
+        basic.showIcon(IconNames.Rollerskate)
+    } else if (IndexNumber == 12) {
+        basic.showIcon(IconNames.Duck)
     } else {
-    	
+        // This is an error condition, should not reach this point ever 
+        for (let index = 0; index < 4; index++) {
+            basic.clearScreen()
+            basic.showIcon(IconNames.Square)
+            basic.clearScreen()
+            basic.showIcon(IconNames.SmallSquare)
+        }
     }
 })
